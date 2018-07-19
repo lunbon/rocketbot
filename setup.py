00f34e5d43f5ef10013d0d0cb8b02519b15fecef
Oleg Korkeshko <rik.san4ez2018@yandex.ru>
2018-07-19 16:04:56 +0500

    bug fixed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ command='python bot.py'
 logging.basicConfig(filename='log.txt', level=logging.INFO)
 while True:
 	try:
-		subprocess.run(['python','bot.py'])
+		subprocess.run(['python3.6','bot.py'])
 	except Exception as ex:
 		logging.error(ex)
 		continue
